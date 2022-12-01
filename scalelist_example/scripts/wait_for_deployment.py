a2d9c786d53c015337992bd06447e24ddc97c196
Anna Szpoton <anna.szpoton@gmail.com>
2022-12-01 10:06:07 +0100

    Add condition dependent on previous deployment

---
+++ scalelist_example/scripts/wait_for_deployment.py
@@ -0,0 +1,22 @@
+from cloudify import ctx
+from cloudify.exceptions import RecoverableError
+from cloudify.manager import get_rest_client
+
+
+def wait_for_deployment(dep_id):
+    client = get_rest_client()
+    executions = client.executions.list(deployment_id=dep_id)
+    running_exec_list = [e for e in executions if e.status not in ['failed', 'terminated', 'cancelled']]
+    if running_exec_list:
+        raise RecoverableError("Waiting for deployment " + dep_id + " to finish all executions")
+
+
+dep_elements = ctx.deployment.id.split('-')
+deployment_index = int(dep_elements[-1])
+prev_deployment_index = deployment_index - 1
+dep_elements = dep_elements[:-1]
+dep_elements.append(str(prev_deployment_index))
+prev_deployment_id = "-".join(dep_elements)
+ctx.logger.info("Previous dep id: " + prev_deployment_id)
+if prev_deployment_index >= 0:
+    wait_for_deployment(prev_deployment_id)
